afcfc7e079c3f5bf30971bed3443a54c27856191
Wizard <nekludov4@gmail.com>
2022-05-02 23:13:25 +0500

    Add new elements in design

---
+++ MyOutfits/app/src/main/java/com/mycompany/myoutfits/fragments/CollectFragment.kt
@@ -0,0 +1,18 @@
+package com.mycompany.myoutfits.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mycompany.myoutfits.R
+
+class CollectFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_collect, container, false)
+    }
+}
